837ef1bfcb3b4809db2c855017489212b70f7b1d
lew+arun@example.com <lew+arun@example.com>
2018-10-09 09:28:47 -0600

    fix_build

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ public class WelcomeController {
 
     private String welcomeMessage;
 
+    @Autowired
     public WelcomeController(@Value("${WELCOME_MESSAGE}") String welcomeMessage){
         this.welcomeMessage = welcomeMessage;
     }
