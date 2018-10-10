71968f1336c82eeb0a5b4d48d70c61f3ed519129
lew+arun@example.com <lew+arun@example.com>
2018-10-10 11:51:31 -0600

    acctuator

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,6 +1,8 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,27 +15,37 @@ public class TimeEntryController {
     public static final String TIME_ENTRY_PATH_URI = TIME_ENTRIES_URI + "/{id}";
 
     @Autowired
+    private final CounterService counter;
+    private final GaugeService gauge;
     TimeEntryRepository timeEntryRepository;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryController(TimeEntryRepository timeEntryRepository,
+                               CounterService counter,
+                               GaugeService gauge) {
         this.timeEntryRepository = timeEntryRepository;
+        this.counter = counter;
+        this.gauge = gauge;
     }
 
     @PostMapping(value = TIME_ENTRIES_URI)
     public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
 
         TimeEntry entry = timeEntryRepository.create(timeEntryToCreate);
+        counter.increment("TimeEntry.created");
+        gauge.submit("timeEntries.count", timeEntryRepository.list().size());
 
         return new ResponseEntity(entry, HttpStatus.CREATED);
     }
 
-   @GetMapping(value = TIME_ENTRY_PATH_URI)
+    @GetMapping(value = TIME_ENTRY_PATH_URI)
     public ResponseEntity<TimeEntry> read(@PathVariable long id) {
         TimeEntry entry = timeEntryRepository.find(id);
 
         HttpStatus status = HttpStatus.OK;
         if (entry == null) {
             status = HttpStatus.NOT_FOUND;
+        } else {
+            counter.increment("TimeEntry.read");
         }
 
         return new ResponseEntity(entry, status);
@@ -42,6 +54,7 @@ public class TimeEntryController {
     @GetMapping(value = TIME_ENTRIES_URI)
     public ResponseEntity<List<TimeEntry>> list() {
         List<TimeEntry> timeEntries = timeEntryRepository.list();
+        counter.increment("TimeEntry.listed");
         return new ResponseEntity(timeEntries, HttpStatus.OK);
     }
 
@@ -49,8 +62,10 @@ public class TimeEntryController {
     public ResponseEntity update(@PathVariable long id, @RequestBody TimeEntry entry) {
         HttpStatus status = HttpStatus.OK;
         TimeEntry updatedEntry = timeEntryRepository.update(id, entry);
-        if(updatedEntry == null){
+        if (updatedEntry == null) {
             status = HttpStatus.NOT_FOUND;
+        } else {
+            counter.increment("TimeEntry.updated");
         }
         return new ResponseEntity(updatedEntry, status);
     }
@@ -59,6 +74,8 @@ public class TimeEntryController {
     public ResponseEntity<TimeEntry> delete(@PathVariable long id) {
         HttpStatus status = HttpStatus.NO_CONTENT;
         timeEntryRepository.delete(id);
+        counter.increment("TimeEntry.deleted");
+        gauge.submit("timeEntries.count", timeEntryRepository.list().size());
         return new ResponseEntity<>(status);
     }
 }
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+    @Autowired
+    TimeEntryRepository timeEntryRepository;
+
+    @Override
+    public Health health() {
+        if(timeEntryRepository.list().size() < 5)
+          return new Health.Builder().up().build();
+        return new Health.Builder().down().build();
+    }
+}
